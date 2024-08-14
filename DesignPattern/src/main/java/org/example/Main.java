f9f103f013bf7b566f264494bc68e0b0ea0ce266
mehmetkirimli <mehmet.kirimli@vbt.com.tr>
2024-08-14 13:54:59 +0300

    first commit

---
+++ DesignPattern/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main
+{
+/**
+ * Sıkıntı , çözümü görememeleri değil ,
+ * Sıkıntı , problemi görememeleridir.
+ **/
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World !");
+  }
+}
